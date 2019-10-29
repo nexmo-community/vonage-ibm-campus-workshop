be5b6d55e16e268349fafc9f0c7600fce592f6cc
Aaron Bassett <me@aaronbassett.com>
2019-10-29 09:40:45 -0700

    Add connect action to NCCO

--- app/server.py
+++ app/server.py
@@ -1,3 +1,5 @@
+import os
+import json
 import tornado.ioloop
 import tornado.web
 from tornado import escape
@@ -7,7 +9,7 @@ from logzero import logfile, logger
 logfile("/tmp/workshop.log", maxBytes=1e6, backupCount=3)
 
 
-class NCCOHandler(tornado.web.RequestHandler):
+class VAPIServer(tornado.web.RequestHandler):
     def write(self, chunk):
         chunk = escape.json_encode(chunk)
         self.set_header("Content-Type", "application/json; charset=UTF-8")
@@ -15,12 +17,49 @@ class NCCOHandler(tornado.web.RequestHandler):
         self._write_buffer.append(chunk)
 
     def get(self):
-        logger.info("Call received")
-        self.write([{"action": "talk", "text": "Hello World"}])
+        to = self.get_argument("to", None, True)
+        logger.info(f"NCCO fetched for call to {to}")
+        self.write(
+            [
+                {
+                    "action": "record",
+                    "eventUrl": [f"{os.environ['SERVER_URL']}/recordings"],
+                },
+                {
+                    "action": "connect",
+                    "eventUrl": [f"{os.environ['SERVER_URL']}"],
+                    "from": os.environ["NEXMO_VIRTUAL_NUMBER"],
+                    "endpoint": [
+                        {
+                            "type": "websocket",
+                            "uri": f"{os.environ['SERVER_URL']}/inbound-call-socket",
+                            "content-type": "audio/l16;rate=16000",
+                            "headers": {},
+                        }
+                    ],
+                },
+            ]
+        )
+
+    def post(self):
+        event = json.loads(self.request.body)
+        logger.info(f"Call to {event['to']} status: {event['status']}")
+        self.write([{"status": "ok"}])
+
+
+class RecordingsServer(tornado.web.RequestHandler):
+    def post(self):
+        recording_meta = json.loads(self.request.body)
+        logger.info(
+            f"New recording available for {recording_meta['conversation_uuid']}"
+        )
+        self.write("OK")
 
 
 def make_app():
-    return tornado.web.Application([(r"/", NCCOHandler)])
+    return tornado.web.Application(
+        [(r"/", VAPIServer), (r"/recordings", RecordingsServer),]
+    )
 
 
 if __name__ == "__main__":
